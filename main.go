1588b0aac045c8656204b0af07be490c615552e9
Mayank Joshi <MAYANKJOSHI.25101280629@gehu.ac.in>
2026-06-09 14:49:16 +0530

    performed initial rituals and made user agent and bounded the result by defining domains

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import(
+	"github.com/PuerkitoBio/goquery"
+	"strings"
+	"net/http"
+	"time"
+	"math/rand"
+	"fmt"
+)
+
+
+
+var BingDomains = map[string]string{  //alll the domains inserted into a map type formate which have jsonish type and it's a key-value pair tho
+	"com":"com", "uk": "co.uk",
+}
+
+type Searchresult struct{
+	ResultRank int
+	ResultURL string
+	ResultTitle string
+	ResultDesc string
+}
+
+var userAgents=[]string{  //a list of browswer it creates, u need to randomize the user ageent, can't use one while making user request
+"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"                          
+}
+
+
+func main() {
+
+
+}
